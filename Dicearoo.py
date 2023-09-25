d513c35caf42357b93740b960712482e868c8d52
Sampson Ho <sampsonho24@gmail.com>
2023-09-25 10:57:03 -0700

    Improving Dicearoo including sleep timing updates

--- Dicearoo.py
+++ Dicearoo.py
@@ -6,32 +6,30 @@ import random
 # Coordinates of a point within the browser window on the left
 x, y = 93, 184  # Replace with the actual coordinates
 
-try:
-    while True:
-        if keyboard.is_pressed('esc'):  # Check if 'Esc' key is pressed
-            print("Esc key pressed. Exiting...")
-            break
+# Move the mouse to the browser and click to focus
+pyautogui.click(x, y)
 
-        # Get the current mouse position
-        original_position = pyautogui.position()
-        
-        # Move the mouse to the browser and click to focus
-        pyautogui.click(x, y)
-            
+i = 0
+try:
+    while keyboard.is_pressed('esc') == False:
+
         # Press F5 to refresh
         pyautogui.press('f5')
         
         # Wait for a short moment for the confirmation dialog to appear
-        time.sleep(random.uniform(0.08, 0.09))  # Random sleep time between 1.1 and 2.3 seconds
+        time.sleep(random.uniform(0.005, 0.01))  # Random sleep time between 1.1 and 2.3 seconds
         
         # Press 'Enter' to confirm refresh
         pyautogui.press('enter')
-
-        # Return the mouse to its original position
-        pyautogui.moveTo(*original_position)
         
         # Wait for a specified random time before the next refresh
-        time.sleep(random.uniform(0.02, 0.05))  # Random sleep time between 1.1 and 2.3 seconds
+        time.sleep(random.uniform(0.05, 0.1))  # Random sleep time between 1.1 and 2.3 seconds
 
+        #Pause on the 20th run to ensure page load
+        if i%25 == 0:
+            time.sleep(2.5) 
+        i += 1
+        print ('run:',i)
 except KeyboardInterrupt:
+
     print("Program terminated by user")
